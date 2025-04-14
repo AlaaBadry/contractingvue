ad90ea194b8a8b1f57a3eb6df67abc27a3dbc2dc
Abdullah <abdollah.500148@gmail.com>
2025-04-14 17:35:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <InvoiceManagement />
+  </div>
+</template>
+
+<script>
+import InvoiceManagement from './components/InvoiceManagement.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InvoiceManagement
+  }
+}
+</script>
+
+<style>
+/* يمكنك إضافة أنماط عامة هنا إذا لزم الأمر */
+</style>
